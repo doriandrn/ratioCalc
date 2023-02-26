32e453fa410ff12e3af8b807e09c22f9405acb41
Dorian Tudorache <dorian.snaz@gmail.com>
2023-02-26 04:19:26 +0200

    bbb

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,185 @@
+<template lang="pug">
+header
+  h1 {{ title }}
+
+main
+  section
+    form
+      span.field(v-for="field, key in rform" :class="key")
+        label(:for="field") {{ key }}
+        input(type="number" v-model="rform[key]" :id="field")
+
+  section.results
+    ul
+      li(v-for="result, i of results")
+        span.inc
+          bar(:data="barData[i]")
+
+footer
+  p &copy snaz4d kkthxbye
+</template>
+
 <script>
 import {
   evaluate, format
 } from 'mathjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
 
+ChartJS.register(CategoryScale, LinearScale, BarElement)
 
 export default {
   data () {
     return {
-      title: 'Ratio Calc',
+      title: 'Ratio Vizualizer',
       rform: {
-        ratio: 2,
-        start: 0,
-        count: 100,
-        ratioCalc: 3
+        ratio: 1.61803398875,
+        startingNumber: 33,
+        displayResults: 7,
+        showNextNumbers: 1
       }
 
     }
+  },
+
+  components: {
+    Bar
+  },
+
+  watch: {
+    results: (a, b, el) => console.log('cc', a, b, el)
   },
 
   computed: {
     results () {
       const r = []
       let c = 0
-      const { start, ratio, count, ratioCalc } = this.rform
-      for (let i = 0; i < count; i++) {
-        const n = Number(start || 0) + i
-        const s = n + ' * ' + String(ratio || 1)
-        const increments = []
-        const c = format(evaluate(s), { precision: 8 })
-        const ii = (c) => format(evaluate(`${c} * ${ratio}`), { precision: 8 })
-        let x = c
-        for (let j = 0; j < ratioCalc; j++) {
+      const { startingNumber, ratio, showNextNumbers, displayResults } = this.rform
+      const ii = (c) => format(evaluate(`${c} * ${ratio || 1}`), { precision: 8 })
+
+      for (let i = 0; i < showNextNumbers; i++) {
+        const n = Number(startingNumber || 0) + i
+        let x = ii(n)
+        const increments = [x]
+        for (let j = 0; j < displayResults; j++) {
           x = ii(x)
           increments.push(x)
         }
-        r.push({ n, c, increments })
+        r.push({ n, increments })
       }
       return r
+    },
+
+    barData () {
+      return this.results.map(res => {
+        const labels = res.increments
+        labels.unshift(res.n)
+        return {
+          labels,
+          datasets: [{ data: labels.map(i => Number(i)) }]
+        }
+      })
     }
   }
 }
 </script>
 
-<template lang="pug">
+
+
+<style scoped lang="stylus">
 header
-  h1 {{ title }}
-
 main
+footer
+  padding 16px 20px
+  width 100%
 
-  section
-    form()
-
-      span.field(v-for="field, key in rform")
-        label(:for="field") {{ key }}
-        input(type="text" v-model="rform[key]" :id="field")
-
-  section
-    h2 Results
-      ul.results
-        li(v-for="result of results")
-          span.bar
-          span.i {{ result.n  }}
-          span.c {{ result.c  }}
-          span.inc
-            span.c(v-for="inc of result.increments") {{ inc }}
+header
+  flex 0 0
 
 footer
-  p &copy snaz4d kkthxbye
-</template>
+  margin-top auto
 
-<style scoped>
-label {
-  display: block
-}
+main
+  display flex
+  flex-flow row wrap
+  gap 20px
+  margin-top 12px
+  flex 1 0
 
-.field {
-  display: inline-block
-}
+label
+  display block
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+input
+  background-color black
+  border 0
+  outline 0
+  padding 8px
+  font-size 16px
+  color #ccc
+  border-radius 2px
 
-section {
-  margin-bottom: 48px
-}
+section
+  flex 1 1
 
-li {
+.ratio
+  input
+    font-weight bold
+
+form
+  display flex
+  flex-flow row wrap
+  gap 8px
+  flex 0 1
+
+.field
+  display block
+
+  &:not(:last-child)
+    margin-bottom 12px
+
+ul
+  list-style-type none
+  padding 0
+
+li
   font-size: 12px;
   display: flex;
-  flex-flow: row nowrap;
-  border-bottom: 1px solid #777;
+  flex-flow: column nowrap;
   position: relative;
-}
+  text-align left
 
-.bar {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-}
+  &:not(:last-child)
+    margin-bottom 8px
 
-li span {
-  min-width: 80px;
-}
-.inc {
-  display: flex;
-  flex-flow: row nowrap;
-}
-span.c {
-  margin-left: 10px;
-  font-weight: bold;
-  text-align: right;
-  width: 100px;
-}
+  span
+    &:not(.inc)
+      display flex
+      flex-flow row nowrap
+      margin-bottom 1px
 
-.r {
+.bar
+  width 100%
+  background blue
+  order 0
+  margin-right auto
+
+.inc
+  display: block
+  width 100%
+
+
+.r
   margin: 0 auto;
-}
+
+.results
+  flex 1 1 auto
+
 </style>
