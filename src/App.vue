830177eb0b3aa0b8a87c2382f6e614c83bb5cfa6
Dorian Tudorache <dorian.snaz@gmail.com>
2023-02-26 16:10:23 +0200

    chartz

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,13 @@ main
     ul
       li(v-for="result, i of results")
         span.inc
-          bar(:data="barData[i]")
+          bar(:data="barData[i]" :options="chartOptions" class="bars")
+          Line(:data="barData[i]" :options="chartOptions" class="line")
+          pie(:data="barData[i]" :options="chartOptions" class="pie")
+          radar(:data="barData[i]" :options="chartOptions" class="radar")
+          PolarArea(:data="barData[i]" :options="chartOptions" class="polar")
+          Doughnut(:data="barData[i]" :options="chartOptions" class="doughnut")
+
 
 footer
   p &copy snaz4d kkthxbye
@@ -30,28 +36,41 @@ import {
   Legend,
   BarElement,
   CategoryScale,
-  LinearScale
+  RadialLinearScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  ArcElement,
+  Colors
 } from 'chart.js'
-import { Bar } from 'vue-chartjs'
+import { Bar, Line, Pie, Doughnut, Bubble, PolarArea, Radar, Scatter } from 'vue-chartjs'
 
-ChartJS.register(CategoryScale, LinearScale, BarElement)
+ChartJS.register(Tooltip, Colors, CategoryScale, LinearScale, RadialLinearScale, BarElement, PointElement, LineElement, ArcElement)
 
 export default {
   data () {
     return {
-      title: 'Ratio Vizualizer',
+      title: 'Ratio Visualizer',
       rform: {
         ratio: 1.61803398875,
         startingNumber: 33,
         displayResults: 7,
         showNextNumbers: 1
-      }
-
+      },
+      chartTypes: ['line', 'bar', 'pie', 'bubble', 'doughnut', 'radar', 'scatter', 'polarArea'],
+      chartType: 'bar'
     }
   },
 
   components: {
-    Bar
+    Bar,
+    Line,
+    Pie,
+    Doughnut,
+    Bubble,
+    PolarArea,
+    Radar,
+    Scatter
   },
 
   watch: {
@@ -69,7 +88,7 @@ export default {
         const n = Number(startingNumber || 0) + i
         let x = ii(n)
         const increments = [x]
-        for (let j = 0; j < displayResults; j++) {
+        for (let j = 0; j < displayResults - 1; j++) {
           x = ii(x)
           increments.push(x)
         }
@@ -87,6 +106,12 @@ export default {
           datasets: [{ data: labels.map(i => Number(i)) }]
         }
       })
+    },
+
+    chartOptions () {
+      return {
+        responsive: true
+      }
     }
   }
 }
@@ -118,6 +143,7 @@ label
   display block
 
 input
+select
   background-color black
   border 0
   outline 0
@@ -125,13 +151,26 @@ input
   font-size 16px
   color #ccc
   border-radius 2px
+  width 100%
 
 section
   flex 1 1
 
+h1
+  font-weight bold
+  font-size 32px
+  line-height 36px
+
 .ratio
   input
     font-weight bold
+
+
+canvas
+  flex 1 1 40%
+  max-width 49%
+  max-height 300px
+  margin-bottom 32px
 
 form
   display flex
@@ -141,6 +180,7 @@ form
 
 .field
   display block
+  flex 1 1 auto
 
   &:not(:last-child)
     margin-bottom 12px
@@ -172,7 +212,9 @@ li
   margin-right auto
 
 .inc
-  display: block
+  display: flex
+  flex-flow row wrap
+  gap 12px
   width 100%
 
 
@@ -180,6 +222,6 @@ li
   margin: 0 auto;
 
 .results
-  flex 1 1 auto
+  flex 1 1 70%
 
 </style>
